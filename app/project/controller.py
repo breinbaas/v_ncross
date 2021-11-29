94fc9e23138d77ed5ccb7b9589d6f1ee51342cf2
Rob van Putten <breinbaasnl@gmail.com>
2021-11-29 17:01:15 +0100

    added params

--- app/project/controller.py
+++ app/project/controller.py
@@ -11,23 +11,40 @@ from .models.crosssection import Crosssection
 class ProjectController(ViktorController):
     label = 'Project controller'
     parametrization = ProjectParametrization
-    crosssections = []
-
+    
     @ParamsFromFile()
     def process_file(self, file: File, **kwargs):  # viktor.core.File
+        crosssections = []
         for line in file.getvalue(encoding='utf-8').splitlines()[1:]:
             crosssection = Crosssection.from_dam_data(line)
             if crosssection is not None:
-                self.crosssections.append(crosssection)
+                crosssections.append(crosssection.json())
+
+        return {
+            'crosssections':crosssections
+        }
 
     @SVGView("SVG plot", duration_guess=1)
     def create_svg_result(self, params, **kwargs):
         fig = plt.figure()
+        zmin = 1e9
+        zmax = -1e9
 
-        for crs in self.crosssections:
+        for crs_json in params.crosssections:
+            crs = Crosssection.parse_raw(crs_json)
             xs = [p.l for p in crs.points]
             zs = [p.z for p in crs.points]
+            zmin = min(zmin, min(zs))
+            zmax = max(zmax, max(zs))
             plt.plot(xs, zs, 'k:')
+
+        xl = params.left_border
+        xr = params.right_border
+        
+        plt.plot([xl, xl],[zmin, zmax], 'r--')
+        plt.plot([xr, xr],[zmin, zmax], 'b--')
+        plt.tight_layout()
+        
 
         # save figure
         svg_data = StringIO()
--- app/project/parametrization.py
+++ app/project/parametrization.py
@@ -1,11 +1,14 @@
-from viktor.parametrization import Parametrization, Section, NumberField
-
-
+from viktor.parametrization import Parametrization, Section, NumberField, HiddenField
 
 
 class ProjectParametrization(Parametrization):
     general = Section('General')
-    general.beam_length = NumberField('Length', suffix='mm', default=2000)
+    general.left_border = NumberField('Border Left', name="left_border", suffix='m', default=0)
+    general.right_border = NumberField('Border Right', name='right_border', suffix='m', default=0)    
+    general.crosssections = HiddenField('CrosssectionsData', name="crosssections")
+
+
+
     
     
 
